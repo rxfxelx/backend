12a7d05d68a54e320d8aaddcbcb5b6a9eb55d2c3
rafael sena <89745814+rxfxelx@users.noreply.github.com>
2025-07-20 01:39:06 -0300

    Delete pac-lead-backend directory

--- pac-lead-backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .product import Product
-
-__all__ = ["User", "Product"]
-
--- pac-lead-backend/app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import router as auth_router
-from .products import router as products_router
-from .webhook import router as webhook_router
-
-__all__ = ["auth_router", "products_router", "webhook_router"]
-
